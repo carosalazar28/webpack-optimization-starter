8d40da2c0e942e553d741f6b726d87eb8e63f112
Carolina <cindy.s.r.28@hotmail.com>
2023-02-16 14:30:27 -0500

    add css modules and minimizer plugin

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,17 +1,49 @@
 const common = require('./webpack.common.config')
 const { merge } = require('webpack-merge')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = merge(common, {
   output: {
     filename: 'js/[name].[contenthash:12].js'
   },
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true }
+            }
+          ]
+        }
+      })
+    ]
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
+        exclude: /\.module\.css$/,
         use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+      },
+      {
+        test: /\.css$/,
+        include: /\.module\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[hash:base64]'
+              }
+            }
+          }
+        ]
       }
     ]
   },
